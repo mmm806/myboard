a2fc38ed98c5820f621eadc3be2d12c999037c27
Kim EungJin <mmm806@naver.com>
2025-04-15 16:19:09 +0900

    feat: dto 완성

---
+++ src/main/java/com/example/myboard/dto/LoginInfo.java
@@ -0,0 +1,28 @@
+package com.example.myboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+public class LoginInfo {
+
+    private int userId;
+    private String email;
+    private String name;
+    private List<String> roles = new ArrayList<>();
+
+    public LoginInfo(int userId, String email, String name) {
+        this.userId = userId;
+        this.email = email;
+        this.name = name;
+    }
+
+    public void addRole(String roleName) {
+        roles.add(roleName);
+    }
+}
---
+++ src/main/java/com/example/myboard/dto/User.java
@@ -0,0 +1,20 @@
+package com.example.myboard.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class User {
+    private int userId;
+    private String email;
+    private String name;
+    private String password;
+    private LocalDateTime regdate;
+}
